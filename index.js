2432169051b16753ff8ec9b24340efa09e952386
unknown <pugalarasan2014@gmail.com>
2023-01-16 15:00:06 +0530

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const https = require("https");
+
+var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("index");
+})
+app.listen(3000, function(){
+   console.log("server is running at port 3000...");
+})
