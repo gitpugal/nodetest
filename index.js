2909ad04e5d0dd6a3c7539cfef289a00f4fd8ca8
unknown <pugalarasan2014@gmail.com>
2023-01-16 15:26:26 +0530

    first

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const https = require("https");
 var app = express();
 app.set("view engine", "ejs");
 
-app.use("/", (req, res) => {
-   res.json({message: "hello vercel koo..."});
+app.get("/", (req, res) => {
+   res.render("index");
 });
 app.listen(3000, function(){
    console.log("server is running at port 3000...");
